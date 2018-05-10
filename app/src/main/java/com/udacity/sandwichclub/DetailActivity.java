e3ab5111a527ea11ab709f5e04c2a4e326fd2f1c
Ryein C. Goddard <ryein@goddardlabs.com>
2018-05-10 19:36:32 -0400

    finished project, json is working and design looks decent enough

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,28 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView mDescriptionTextView;
+    private TextView mPlaceOfOriginTextView;
+    private TextView mPlaceOfOriginLabelTextView;
+    private TextView mAlsoKnownAsTextView;
+    private TextView mAlsoKnownLabelTextView;
+    private TextView mIngredientsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,12 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mDescriptionTextView =   findViewById(R.id.description_tv);
+        mPlaceOfOriginTextView = findViewById(R.id.origin_tv);
+        mPlaceOfOriginLabelTextView = findViewById(R.id.origin_label);
+        mAlsoKnownAsTextView =  findViewById(R.id.also_known_tv);
+        mAlsoKnownLabelTextView = findViewById(R.id.also_known_label);
+        mIngredientsTextView =  findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +60,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +73,43 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        mDescriptionTextView.setText(sandwich.getDescription());
 
+        if (sandwich.getPlaceOfOrigin().isEmpty()){
+            mPlaceOfOriginLabelTextView.setVisibility(View.GONE);
+            mPlaceOfOriginTextView.setVisibility(View.GONE);
+        }else {
+            mPlaceOfOriginLabelTextView.setVisibility(View.VISIBLE);
+            mPlaceOfOriginTextView.setVisibility(View.VISIBLE);
+            mPlaceOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        if (sandwich.getAlsoKnownAs().size() == 0){
+            mAlsoKnownAsTextView.setVisibility(View.GONE);
+            mAlsoKnownLabelTextView.setVisibility(View.GONE);
+        }else {
+            mAlsoKnownAsTextView.setVisibility(View.VISIBLE);
+            mAlsoKnownLabelTextView.setVisibility(View.VISIBLE);
+            String formattedAliases = listToString(sandwich.getAlsoKnownAs());
+            mAlsoKnownAsTextView.setText(formattedAliases);
+        }
+
+        String formattedIngredients = listToString(sandwich.getIngredients());
+        mIngredientsTextView.setText(formattedIngredients);
+    }
+
+    private String listToString(List<String> items){
+        String result = "";
+
+        StringBuilder t = new StringBuilder();
+        for(int i=0; i <= items.size()-1; i++) {
+            t.append(items.get(i));
+            if(i != items.size()-1)
+                t.append(", ");
+        }
+        result = t.toString();
+
+        return result;
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,66 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
-public class JsonUtils {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+    private static final String TAG = "JsonUtils";
+
+    public static Sandwich parseSandwichJson(String jsonString) {
+        if (jsonString == null) {
+            return null;
+        }
+
+        final String nameField = "name";
+        final String mainNameField = "mainName";
+        final String imageField = "image";
+        final String alsoKnownAsField = "alsoKnownAs";
+        final String placeOfOriginField = "placeOfOrigin";
+        final String descriptionField = "description";
+        final String ingredientsField = "ingredients";
+
+        try{
+            Sandwich sandwich = new Sandwich();
+
+            JSONObject jsonObject = new JSONObject(jsonString);
+
+            JSONObject nameObject = jsonObject.getJSONObject(nameField);
+            sandwich.setMainName(nameObject.getString(mainNameField));
+
+            sandwich.setImage(jsonObject.getString(imageField));
+
+            JSONArray alsoKnownAsNamesArray = nameObject.getJSONArray(alsoKnownAsField);
+            sandwich.setAlsoKnownAs(jsonToList(alsoKnownAsNamesArray));
+            sandwich.setPlaceOfOrigin(jsonObject.getString(placeOfOriginField));
+            sandwich.setDescription(jsonObject.getString(descriptionField));
+
+            JSONArray ingredientsArray = jsonObject.getJSONArray(ingredientsField);
+            sandwich.setIngredients(jsonToList(ingredientsArray));
+
+            return sandwich;
+
+        } catch (final JSONException e){
+            Log.e(TAG, "JSON error: " + e);
+            return null;
+        }
+    }
+
+    static private List<String> jsonToList(JSONArray jsonArray) throws JSONException {
+        List<String> result = new ArrayList<>();
+
+        if ( jsonArray != null){
+            for (int i=0; i< jsonArray.length(); i++){
+                result.add(jsonArray.getString(i));
+            }
+        }
+        return result;
     }
 }
